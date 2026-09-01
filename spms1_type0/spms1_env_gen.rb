edbb390b9610ee8f030f1a2b2eb1257bb2d57118
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2026-09-01 20:54:24 +0900

    type-0: Improve code readability in EnvGen by adding blank lines

--- spms1_type0/spms1_env_gen.rb
+++ spms1_type0/spms1_env_gen.rb
@@ -20,14 +20,17 @@ module Spms1
       @sample_rate = sample_rate
       @state = STATE_IDLE
       @current_level = 0.0
+
       @attack = 0.0
       @decay = 0.0
       @sustain = 1.0
       @attack_target = 2.0
+
       @was_gate_on = false
       @attack_coef = 1.0
       @decay_coef = 1.0
       @sample_counter = 0
+
       update_coefficients_full
     end
 
@@ -54,21 +57,30 @@ module Spms1
         is_gate_on = gate_input >= 0.5
         gate_rose = is_gate_on && !@was_gate_on
         gate_fell = !is_gate_on && @was_gate_on
+
         @state = gate_rose ? STATE_ATTACK : (gate_fell ? STATE_SUSTAIN : @state)
         @was_gate_on = is_gate_on
+
         update_coefficients_full
+
         target = (@state == STATE_ATTACK) ? @attack_target : (((@state == STATE_SUSTAIN) && is_gate_on) ? @sustain : 0.0)
         coef = (@state == STATE_ATTACK) ? @attack_coef : ((@state == STATE_SUSTAIN) ? @decay_coef : 0.0)
+
         apply_step = (@state != STATE_SUSTAIN) || !is_gate_on || (@sustain < @current_level)
         coef_masked = apply_step ? coef : 0.0
+
         @current_level += (target - @current_level) * coef_masked
+
         is_attack_done = (@state == STATE_ATTACK) && (@current_level >= 1.0 || !@was_gate_on)
         is_idle_reached = (@state == STATE_SUSTAIN) && !@was_gate_on && (@current_level < 1e-5)
         is_forced_attack = (@state == STATE_IDLE) && @was_gate_on
+
         @state = is_attack_done ? STATE_SUSTAIN : (is_idle_reached ? STATE_IDLE : (is_forced_attack ? STATE_ATTACK : @state))
+
         @current_level = 1.0 if is_attack_done
         @current_level = 0.0 if is_idle_reached || (@state == STATE_IDLE && !@was_gate_on)
       end
+
       @sample_counter = (@sample_counter + 1) & 3
       @current_level
     end
@@ -77,6 +89,7 @@ module Spms1
 
     def update_coefficients_full
       effective_rate = @sample_rate * (1.0 / 4.0)
+
       @attack_coef = 1.0 / (ATTACK_BASE * calculate_exp_fast(@attack) * effective_rate)
       @decay_coef  = 1.0 / (DECAY_BASE  * calculate_exp_fast(@decay)  * effective_rate)
     end
@@ -85,8 +98,10 @@ module Spms1
       v_scale = value * 128.0
       index = v_scale.to_i
       fraction = v_scale - index.to_f
+
       e0 = EXP_TABLE[index]
       e1 = EXP_TABLE[index + 1]
+
       e0 + fraction * (e1 - e0)
     end
   end
